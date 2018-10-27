ef7d12b9602d2463f854e537a3e7304e94493671
hpzhang <zhanghaopai@outlook.com>
2018-10-28 00:01:19 +0800

    第二个项目
    第二个项目，主要是添加了三个控件。

---
+++ 3_1_1/3_1_1/Default.aspx
@@ -0,0 +1,19 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_3_1_1.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:TextBox ID="TextBox1" runat="server" ></asp:TextBox>
+        <input id="Button1" type="button" value="Click Me!" runat="server" />
+        <asp:Label ID="Label1" runat="server" Text="Label"></asp:Label>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 3_1_1/3_1_1/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_1_1
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            Button1.Value = "Click Me Now!";
+            Label1.Text = "<h1>hello world</h1>";
+            Image myImage = new Image();
+            myImage.AlternateText = "Image Text";
+            myImage.ImageUrl = "img1.jpg";
+            myImage.ImageAlign = ImageAlign.Left;
+            Page.Controls.Add(myImage);
+
+        }
+    }
+}
