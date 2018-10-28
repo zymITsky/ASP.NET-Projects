b4133e06f005461b3ec6821849f39239c71dbdb2
hpzhang <zhanghaopai@outlook.com>
2018-10-28 19:16:28 +0800

    第三个小项目
    使用了第三方控件，对于如何引入第三方控件，问题遗留为如何使用第三方控件和如何开发第三方控件

---
+++ 3_1_5/3_1_5/Default.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="_3_1_5.Default" %>
+
+<%@ Register assembly="WebValidates" namespace="WebValidates" tagprefix="cc1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <div>用户名：<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox><br />
+            验证码：<asp:TextBox ID="TextBox2" runat="server"></asp:TextBox>
+            <cc1:ValidateCode ID="ValidateCode1" runat="server" /><br />
+            <asp:Button ID="Button1" runat="server" Text="Button" OnClick="Button1_Click" />
+            <asp:Label ID="Label1" runat="server" Visible="False"></asp:Label><br />
+        </div>
+    </div>
+    </form>
+</body>
+</html>
---
+++ 3_1_5/3_1_5/Default.aspx.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace _3_1_5
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                ValidateCode1.CreateYzm();
+            }
+        }
+        protected bool CheckCode() {
+            if (ValidateCode1.CheckYzm(TextBox2.Text)){
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (!CheckCode())
+            {
+                this.Label1.Visible = true;
+                this.Label1.Text = "验证码错误";
+            }
+            else {
+                this.Label1.Visible = true;
+                this.Label1.Text = "验证通过！";
+            }
+        }
+    }
+}
